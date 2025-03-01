d485525dea79d96a5a1a94f1d3550583171bf8fe
copter50029 <152718101+copter50029@users.noreply.github.com>
2025-03-01 15:18:16 +0700

    forgot to save

--- dags/kafka_steam.py
+++ dags/kafka_steam.py
@@ -37,14 +37,36 @@ def steam_data():
     print(json.dumps(data, indent=4))
     
 
-# with DAG('airpollution',
-#          default_args=default_arg,
-#          schedule_interval='@daily',
-#          catchup=False) as dag:
-    
-#     steaming_task = PythonOperator(
-#         task_id='stream_data_from_api',
-#         python_callable=steam_data
-#     )
-
-steam_data()
+def format_data(res):
+    data = {}
+    data['timestamp'] = res['list'][0]['dt']
+    data['lon'] = res['coord']['lon']
+    data['lat'] = res['coord']['lat']
+    data['AQI'] = res['list'][0]['main']['aqi']
+    data['CO'] = res['list'][0]['components']['co']
+    data['NO'] = res['list'][0]['components']['no']
+    data['NO2'] = res['list'][0]['components']['no2']
+    data['O3'] = res['list'][0]['components']['o3']
+    data['SO2'] = res['list'][0]['components']['so2']
+    data['PM2_5'] = res['list'][0]['components']['pm2_5']
+    data['PM10'] = res['list'][0]['components']['pm10']
+    data['NH3'] = res['list'][0]['components']['nh3']
+    return data
+def steam_data():
+    import logging
+    # print(json.dumps(data, indent=4))
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000) # connect to kafka
+    #send data to kafka every 3mins
+    curr_time = time.time()
+    while True:
+        if time.time() > curr_time + 100:
+            break
+        try:
+            res = get_data()
+            data = format_data(res)
+            producer.send('airpollution', json.dumps(data).encode('utf-8'))
+            logging.info('Data sent to kafka')
+            time.sleep(100) # sleep for 100s
+        except Exception as e:
+            logging.error(e)
+            continue
